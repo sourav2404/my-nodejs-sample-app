755dc94ca96376bf5fe69731a565c7ae91ead0b2
Sourav Mehra <soumehra@in.ibm.com>
2023-05-31 11:14:04 +0530

    Upgraded package.json and server.js test
    Upgraded package.json and server.js test

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const port = 8080
 app.use(cors())
 app.use(express.json())
 
-app.use('/hello', function (req, res) {res.send("ABLE TO ACCESS APPLICATION")});
+app.use('/hello', function (req, res) {res.send("ABLE TO ACCESS NODE.JS APPLICATION")});
 
 app.listen(port, () => console.log(`BOX-CLIENT-SERVICE APPs listening on port ${port}!`));
